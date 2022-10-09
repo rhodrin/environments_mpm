57934ea90d40c8ea3847a7aa93c178255b88aec6
acse-68e9c9dc <hanmengrui1112@163.com>
2022-10-09 11:01:39 +0100

    <my commit message>

--- scripts/smooth_image.py
+++ scripts/smooth_image.py
@@ -1,5 +1,18 @@
-from envtest import rand_array
+from email.mime import image
+from envtest import smooth_image
+from scipy import misc
+import matplotlib.pyplot as plt
 
-shape = (3, 3)
+image = misc.ascent()
+sigma = 5
+
+smoothed_image = smooth_image(image, sigma)
+
+f = plt.figure()
+f.add_subplot(1,2,1)
+plt.imshow(image)
+f.add_subplot(1,2,2)
+plt.imshow(smoothed_image)
+plt.show(block=True)
 
 print(rand_array(shape))
