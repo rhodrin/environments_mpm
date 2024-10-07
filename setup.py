b1a9516043cc3b43798469c106e3e5b5bf1eb69f
rhodrin <rhodri.nelson@gmail.com>
2024-10-07 14:04:30 +0100

    Setup updates.

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='envtest',
-    use_scm_version=True,
+    use_scm_version={
+        "write_to": "envtest/_version.py",  # Optional: writes the version to a file
+        "version_scheme": "no-guess-dev",   # Version scheme that doesn't require tags
+        "local_scheme": "node-and-date",    # Use commit hash and date for local version
+    },
     setup_requires=['setuptools_scm'],
     description="Playing with virtual environments.",
     long_description="Playing with virtual environments.",
     author="Imperial College London",
     author_email='rhodri.nelson@imperial.ac.uk',
-    packages=['envtest'],
+    packages=find_packages(),
 )
-
-
