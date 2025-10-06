25447d34ea1750d457d782fda227d7fd1108998b
rhodrin <rhodri.nelson@gmail.com>
2025-10-06 11:56:07 +0100

    Update package.

--- envtest/builtins.py
+++ envtest/builtins.py
@@ -1,8 +1,15 @@
 import numpy as np
+from scipy.ndimage import gaussian_filter
+from scipy import misc
 
-
-__all__ = ['rand_array']
+__all__ = ['rand_array', 'smooth_image', 'my_mat_solve']
 
 
 def rand_array(shape):
     return np.random.rand(*shape)
+
+def smooth_image(a, sigma=1):
+    return gaussian_filter(a, sigma=sigma)
+
+def my_mat_solve(A, b):
+    return A.inv()*b
